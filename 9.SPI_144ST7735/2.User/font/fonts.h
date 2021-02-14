94488b2b34affcd5cd49cd69b33ed356bd98462c
MomentDerek <momincong@foxmail.com>
2021-02-14 20:21:57 +0800

    Add SPI_144ST7735

---
+++ 9.SPI_144ST7735/2.User/font/fonts.h
@@ -0,0 +1,33 @@
+/**
+  ******************************************************************************
+  * @author  
+  * @version V1.0.0
+  * @date    2020/07/1
+  ******************************************************************************
+	* @attention 
+  ******************************************************************************
+  */
+
+#ifndef __FONT_H
+#define __FONT_H       
+
+#include "gd32e10x.h"
+
+
+/** @defgroup FONTS_Exported_Types
+  * @{
+  */ 
+typedef struct _tFont
+{    
+  const uint8_t *table;
+  uint16_t Word_width;
+  uint16_t Word_height;
+  
+}sFONT;
+
+extern sFONT Font24x32;
+extern sFONT Font16x24;
+extern sFONT Font8x16;
+
+
+#endif
